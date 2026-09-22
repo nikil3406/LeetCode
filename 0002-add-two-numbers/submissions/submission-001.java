7a44cbd4b86346d76c5f1df7b74098bfb3bad749
nikil3406 <nikilreddy3406@gmail.com>
2026-09-22 18:05:26 +0530

    Add LeetCode submission 1925092769 for #2

---
+++ 0002-add-two-numbers/submissions/submission-001.java
@@ -0,0 +1,46 @@
+# LeetCode Submission
+# Submission ID: 1925092769
+# Status: Accepted
+# Language: java
+# Runtime: 1 ms
+# Memory: 46.7 MB
+# Submitted: 2026-02-20 06:26:57 UTC
+#
+# This file contains the actual code submitted to LeetCode.
+#
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode current = temp;
+        int carry = 0;
+
+        while(l1!=null || l2!=null || carry!=0){
+            int sum = carry;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+
+            carry = sum/10;
+            sum=sum%10;
+            current.next = new ListNode(sum);
+            current = current.next;
+        }
+        return temp.next;
+    }
+}
